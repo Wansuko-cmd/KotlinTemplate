d1c0d24b607acf12efab05b8ed3d6d968894b358
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2024-11-24 18:05:11 +0900

    [add] version catalogを追加

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,25 @@
-rootProject.name = "KotlinTemplate"
+@file:Suppress("UnstableApiUsage")
 
+pluginManagement {
+    includeBuild("build-logic")
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+    versionCatalogs {
+        create("projects") {
+            from(files("./gradle/projects.versions.toml"))
+        }
+    }
+}
+
+rootProject.name = "KotlinTemplate"
