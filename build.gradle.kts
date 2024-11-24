9e66f846995d22c7687351341e724f0be4497ac6
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2024-11-24 18:29:51 +0900

    [add] ktlintの設定を追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,11 @@
 plugins {
     kotlin("jvm") version libs.versions.kotlin apply false
+    alias(projects.plugins.ktlint)
+}
+
+buildscript {
+    dependencies {
+        classpath(libs.gradle.kotlin)
+        classpath(libs.gradle.ksp)
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,3 +23,5 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "KotlinTemplate"
+
+include(":lib")
